254713da98972c22ef883d51ec27397a58fa6c2d
IParsons1000 <iparsons1000+github@gmail.com>
2026-05-06 11:26:53 -0700

    Add in dummy build constants file to satisfy github actions

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "advantage-kit-clojure";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 6;
+  public static final String GIT_SHA = "d55519dc85df61bbbd91090978145634b5a55eea";
+  public static final String GIT_DATE = "2026-05-04 14:15:41 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-05-04 15:20:46 EDT";
+  public static final long BUILD_UNIX_TIME = 1777922446784L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
